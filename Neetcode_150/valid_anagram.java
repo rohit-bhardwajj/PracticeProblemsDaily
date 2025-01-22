a58e1508a438d10a4161ff0733429b84d3919654
Rohit Bhardwaj <154592665+rohit-bhardwajj@users.noreply.github.com>
2025-01-22 11:49:13 +0530

    add 2nd sum

--- Neetcode_150/valid_anagram.java
+++ Neetcode_150/valid_anagram.java
@@ -1,11 +1,32 @@
+//optimal solution
 import java.util.*;
 class Solution {
-    public String makeAnagram(String s){
-        char arr[] = s.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
-    }
+   
     public boolean isAnagram(String s, String l) {
-         return makeAnagram(s).equals(makeAnagram(l));
+        if(s.length()!=l.length()){
+            return false;
+        }
+         char arr[]= new char[26];
+         for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[l.charAt(i)-'a']--;
+         }
+         for(int count:arr){
+            if(count!=0)
+            return false;
+         }
+         return true;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public String makeAnagram(String s){
+//         char arr[] = s.toCharArray();
+//         Arrays.sort(arr);
+//         return new String(arr);
+//     }
+//     public boolean isAnagram(String s, String l) {
+//          return makeAnagram(s).equals(makeAnagram(l));
+//     }
+// }
