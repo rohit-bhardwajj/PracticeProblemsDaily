1cdaa9433d1e42c2e4c68f1fda47634318f85dd9
Rohit Bhardwaj <154592665+rohit-bhardwajj@users.noreply.github.com>
2025-02-11 20:22:33 +0530

    updated solution

--- Neetcode_150/LongestSubstringWithoutRepeatingChars.java
+++ Neetcode_150/LongestSubstringWithoutRepeatingChars.java
@@ -1,3 +1,31 @@
+//more optimal approach using array of indexes
+class Solution{
+    public int LongestStrWithtRepChar(String s){
+        int l=0; int r=0;
+        int count = 0; int maxC= 0;
+        int hasharr[] = new int[256];
+        Arrays.fill(hasharr,-1);//to mark all chars unseen
+        //as we encounter a char we'll mark its place in arr by its recent index at which it was seen
+        while(r<s.length()){
+            char currentChar = s.charAt(r);
+            if(hasharr[currentChar]!=-1 &&/*means char is coming another time*/
+            hasharr[currentChar]>=l){/*means char is part of currnt substring*/
+        //we'll need to remove its prev occ from currnt substring
+            l = hasharr[currentChar] /*its prev index */ + 1;
+            hasharr[currentChar] = r; //update its new index value
+            }
+            //else if char is coming first time in substring
+            //assign its place in array as its index
+            hasharr[currentChar]= r;
+            count = r-l+1;
+            maxC = Math.max(maxC,count);
+        }
+        return maxC;
+    }
+}
+
+
+//using hashmap
 class Solution{
     public int LongestStrWithtRepChar(String s){
         //first solution-using hashmap
