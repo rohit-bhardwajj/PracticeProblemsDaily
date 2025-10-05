349642bbc7305a009ef4081cd7acef0300a6bc9c
Rohit Bhardwaj <rohitkumar01498@gmail.com>
2025-10-05 20:10:34 +0530

    improved soln

--- Sprint-pace/Day-6/LC349_IntersectionOfArr.java
+++ Sprint-pace/Day-6/LC349_IntersectionOfArr.java
@@ -1,3 +1,24 @@
+//sol 2
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                list.add(nums2[i]);
+                set.remove(nums2[i]);
+            }
+        }
+        int arr[] = new int[list.size()]; int i=0;
+        for(int n:list){
+            arr[i++] = n;
+        }
+        return arr;
+    }
+}
 //sol 1 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
